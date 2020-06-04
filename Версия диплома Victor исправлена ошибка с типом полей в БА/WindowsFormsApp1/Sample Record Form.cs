0598fd124e7508426632362f5bc7c90892cac0e6
Victor564 <44581465+Victor564@users.noreply.github.com>
2020-06-04 19:18:51 +0300

    в

--- Версия диплома Victor исправлена ошибка с типом полей в БА/WindowsFormsApp1/Sample Record Form.cs
+++ Версия диплома Victor исправлена ошибка с типом полей в БА/WindowsFormsApp1/Sample Record Form.cs
@@ -18,6 +18,8 @@ namespace WindowsFormsApp1
         String data;
         String dd, mm, gg;
         public string operation;
+        string file = "IDNTFParams.dat";
+        idntf ogr = new idntf();           
         //-------------------------------------------конец Области переменных ---------------------------------------------------//
         private Proby mainform;
         public Sample_Record_Form(Proby mainform)
@@ -103,27 +105,90 @@ namespace WindowsFormsApp1
 
         private void Sample_Record_Form_Load(object sender, EventArgs e)
         {
-            textBox1.Text = Convert.ToString(zapis[0]);
-            textBox2.Text = Convert.ToString(zapis[1]);
-            textBox5.Text = Convert.ToString(zapis[2]);
-            textBox9.Text = Convert.ToString(zapis[3]);
-            textBox3.Text = Convert.ToString(zapis[4]);
-            textBox4.Text = Convert.ToString(zapis[5]);
-            textBox7.Text = Convert.ToString(zapis[6]);
-            textBox6.Text = Convert.ToString(zapis[7]);
-            textBox8.Text = Convert.ToString(zapis[8]);
-            textBox13.Text = Convert.ToString(zapis[9]);
-            textBox14.Text = Convert.ToString(zapis[10]);
-            textBox15.Text = Convert.ToString(zapis[11]);
-            textBox17.Text = Convert.ToString(zapis[12]);
-
-            textBox16.Text = Convert.ToString(zapis[13]);
-
-            textBox18.Text = Convert.ToString(zapis[14]);
-            textBox11.Text = Convert.ToString(zapis[15]);
-            textBox10.Text = Convert.ToString(zapis[16]);
-            textBox12.Text = Convert.ToString(zapis[17]);
-            richTextBox1.Text = Convert.ToString(zapis[18]);
+            textBox1.Text = Convert.ToString(zapis[0]); //ид пробы
+            textBox2.Text = Convert.ToString(zapis[1]);// ид записи
+            textBox5.Text = Convert.ToString(zapis[2]);//порядковый номер
+            textBox9.Text = Convert.ToString(zapis[3]); //средняя длина 
+            textBox3.Text = Convert.ToString(zapis[4]); //минимальный размер
+            textBox4.Text = Convert.ToString(zapis[5]); //максимальный размер
+            textBox7.Text = Convert.ToString(zapis[6]); //количество рыб
+            textBox6.Text = Convert.ToString(zapis[7]); //масса рыбы
+            textBox8.Text = Convert.ToString(zapis[8]); //средняя масса рыбы
+            textBox13.Text = Convert.ToString(zapis[9]); //номер бюкса
+            textBox14.Text = Convert.ToString(zapis[10]); //вес бюкса
+            textBox15.Text = Convert.ToString(zapis[11]); //сырая масса бюкса с навеской
+            textBox17.Text = Convert.ToString(zapis[12]); //масса навески
+            textBox16.Text = Convert.ToString(zapis[13]); //сухая масса бюкса с навеской 
+            textBox18.Text = Convert.ToString(zapis[14]);  //масса сухой навески
+            textBox11.Text = Convert.ToString(zapis[15]); //содержание СВ
+            textBox10.Text = Convert.ToString(zapis[16]); //содержание липидов
+            textBox12.Text = Convert.ToString(zapis[17]); //содержание липидов расчетное
+            richTextBox1.Text = Convert.ToString(zapis[18]); //комментарий 
         }
+    }
+    public class idntf
+    {
+        private string idnt;
+        private Double SizeMax, SizeMin, MaxTLFLSL, MinTLFLSL, MaxWetWeight, MinWetWeight, MaxBoxWeight, MinBoxWeight,
+            MaxGrosWW, MinGrosWW, MaxGrosDW, MinGrosDW, MaxDW, MinDW, MaxTL, MinTL, x1, x2;
+
+        public idntf()
+        {
+            idnt = "N/A";
+        }
+        public idntf(string idnt, double SizeMax, double SizeMin, double MaxTLFLSL, double MinTLFLSL, double MaxWetWeight, double MinWetWeight,
+          double MaxBoxWeight, double MinBoxWeight, double MaxGrosWW, double MinGrosWW, double MaxGrosDW, double MinGrosDW, double MaxDW,
+          double MinDW, double MaxTL, double MinTL, double x1, double x2)
+        {
+            this.idnt = idnt;
+            this.SizeMax = SizeMax;
+            this.SizeMin = SizeMin;
+            this.MaxTLFLSL = MaxTLFLSL;
+            this.MinTLFLSL = MinTLFLSL;
+            this.MaxWetWeight = MaxWetWeight;
+            this.MinWetWeight = MinWetWeight;
+            this.MaxBoxWeight = MaxBoxWeight;
+            this.MinBoxWeight = MinBoxWeight;
+            this.MaxGrosWW = MaxGrosWW;
+            this.MinGrosWW = MinGrosWW;
+            this.MaxGrosDW = MaxGrosDW;
+            this.MinGrosDW = MinGrosDW;
+            this.MaxDW = MaxDW;
+            this.MinDW = MinDW;
+            this.MaxTL = MaxTL;
+            this.MinTL = MinTL;
+            this.x1 = x1;
+            this.x2 = x2;
+        }
+        private ArrayList idntparams = new ArrayList();
+        public ArrayList GetIdnt()
+        {
+            idntparams.Add(idnt);
+            idntparams.Add(SizeMax);
+            idntparams.Add(SizeMin);
+            idntparams.Add(MaxTLFLSL);
+            idntparams.Add(MinTLFLSL);
+            idntparams.Add(MaxWetWeight);
+            idntparams.Add(MinWetWeight);
+            idntparams.Add(MaxBoxWeight);
+            idntparams.Add(MinBoxWeight);
+            idntparams.Add(MaxGrosWW);
+            idntparams.Add(MinGrosWW);
+            idntparams.Add(MaxGrosDW);
+            idntparams.Add(MinGrosDW);
+            idntparams.Add(MaxDW);
+            idntparams.Add(MinDW);
+            idntparams.Add(MaxTL);
+            idntparams.Add(MinTL);
+            idntparams.Add(x1);
+            idntparams.Add(x2);
+            return idntparams;
+        }
+        public string IDent()
+        {
+            return idnt;
+        }
+
+        //класс надо переписать, добавить все переменные и пусть он возвращает их (https://docs.microsoft.com/ru-ru/dotnet/csharp/language-reference/keywords/class)
     }
 }
